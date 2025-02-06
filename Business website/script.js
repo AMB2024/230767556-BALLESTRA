e76fa7d5f3c7f42630e5c6a6abfbac1d85e8fea4
AMB2024 <amb7556@students.uc-bcf.edu.ph>
2025-02-06 12:48:39 +0800

    final

--- Business website/script.js
+++
@@ -1,5 +0,0 @@
-// Toggle the mobile navigation menu on burger click
-function toggleMenu() {
-    const navLinks = document.querySelector('.nav-links');
-    navLinks.classList.toggle('active');
-}
